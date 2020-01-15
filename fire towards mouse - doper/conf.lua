dd43989cf99f08522913e96bb5f8cfd1fa143f4a
dave-gu <59486439+dave-gu@users.noreply.github.com>
2020-01-15 01:47:14 -0500

    Adding files.
    Added all files.

---
+++ fire towards mouse - doper/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+        t.title = "Controls - wasd LButton RButton qe"
+        t.version = "0.10.1"
+        t.window.width = 600
+        t.window.height = 600
+        t.console = true
+end
+
---
+++ run.bat
@@ -0,0 +1,1 @@
+love.exe "fire towards mouse - doper"
